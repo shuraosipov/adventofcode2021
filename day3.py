3a18ab90d24fa4f7a198005b9403b65d87a1408a
Alexander Osipov <shuraosipov@gmail.com>
2021-12-05 22:34:03 -0500

    cleanup

--- day3.py
+++ day3.py
@@ -1,9 +1,5 @@
 # What are the power consumption and life support rating of the submarine?
-
 import pandas as pd 
-
-def convert_binary_to_decimal(binary_number):
-    return int(int("".join(binary_number), 2))
 
 def get_gamma_epsilon_rate(df):
     gamma_rate = []
@@ -13,45 +9,37 @@ def get_gamma_epsilon_rate(df):
         gamma_rate.append(df[col].value_counts().idxmax())
         epsilon_rate.append(df[col].value_counts().idxmin())
 
-    gamma_rate_decimal = int(int("".join(gamma_rate), 2))
-    epsilon_rate_decimal = int(int("".join(epsilon_rate), 2))
+    gamma_rate_decimal = convert_binary_to_decimal(gamma_rate)
+    epsilon_rate_decimal = convert_binary_to_decimal(epsilon_rate)
 
     return (gamma_rate_decimal, epsilon_rate_decimal)
 
+def convert_binary_to_decimal(binary_number):
+    return int(int("".join(binary_number), 2))
 
 def calculate_power_consumption(gamma_epsilon_rate):
     return gamma_epsilon_rate[0] * gamma_epsilon_rate[1]
 
-
 def find_oxygen_generator_rating(df):
     for col in df.columns:
-        if df[col].count() == 2:
-            df = df.loc[df[col].str.startswith('1')]
-        elif df[col].count() == 1:
-            break
-        else:
-            if df[df[col] == '1'].shape[0] == df[df[col] == '0'].shape[0]:
-                 df = df.loc[df[col].str.startswith('1')]
-            elif df[col].value_counts().idxmax() == '1':
+        if df[df[col] == '1'].shape[0] == df[df[col] == '0'].shape[0]:
                 df = df.loc[df[col].str.startswith('1')]
-            elif df[col].value_counts().idxmax() == '0':
-                df = df.loc[df[col].str.startswith('0')]
+        elif df[col].value_counts().idxmax() == '1':
+            df = df.loc[df[col].str.startswith('1')]
+        elif df[col].value_counts().idxmax() == '0':
+            df = df.loc[df[col].str.startswith('0')]
+    
     return df.to_string(header=False,index=False,index_names=False).split(' ')
 
 def find_CO2_scrubber_rating(df):
     for col in df.columns:
-
-        if df[col].count() == 2:
+        if df[df[col] == '1'].shape[0] == df[df[col] == '0'].shape[0]:
             df = df.loc[df[col].str.startswith('0')]
-        elif df[col].count() == 1:
-            break
-        else:
-            if df[df[col] == '1'].shape[0] == df[df[col] == '0'].shape[0]:
-                df = df.loc[df[col].str.startswith('0')]
-            elif df[col].value_counts().idxmin() == '1':
-                df = df.loc[df[col].str.startswith('1')]
-            elif df[col].value_counts().idxmin() == '0':
-                df = df.loc[df[col].str.startswith('0')]
+        elif df[col].value_counts().idxmin() == '1':
+            df = df.loc[df[col].str.startswith('1')]
+        elif df[col].value_counts().idxmin() == '0':
+            df = df.loc[df[col].str.startswith('0')]
+
     return df.to_string(header=False,index=False,index_names=False).split(' ')
 
 def find_life_support_rating(oxygen_generator_rating, CO2_scrubber_rating):
@@ -74,7 +62,6 @@ print("Power consumption of the submarine is", power_consumption)
 """
 Part Two
 """
-
 oxygen_generator_rating_binary = find_oxygen_generator_rating(df)
 oxygen_generator_rating_decimal = convert_binary_to_decimal(oxygen_generator_rating_binary)
 

e417a64973059ef58f18441645a98179b65a2e82
Alexander Osipov <shuraosipov@gmail.com>
2021-12-06 23:28:29 -0500

    day 4

---
+++ day4.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+
+def mark_number(board, number):
+    index = get_index(board, number)
+    board[index[0][0],index[1][0]] = 999
+
+def get_index(board, number):
+    return np.where(board == number)
+
+def check_row_is_complete(board, number):
+    # Check rows in which all values are equal
+    for i in range(board.shape[0]):
+        if np.all(board[i] == 999):
+            #print('Row: ', i)
+            #print("Winner is", board)
+            #print("Winning number", number)
+            return True
+
+def check_col_is_complete(board, number):
+    # Check Columns in which all values are equal
+    trans_arr = board.T
+    #print(trans_arr)
+    for i in range(trans_arr.shape[0]):
+        if np.all(trans_arr[i] == 999):
+            #print('Column: ', i)
+            #print("Winner is", board)
+            #print("Winning number", number)
+            return True
+
+def play_game(numbers, board, board_id):
+    results = {}
+    iteration = 1
+
+    for number in numbers:
+        
+        if number in board:
+            mark_number(board, number)
+            if check_row_is_complete(board, number) or check_col_is_complete(board, number):
+                results[board_id] = {
+                    'board_data' : board, 
+                    'winning_number' : number,
+                    'iteration': iteration
+                }
+                return results
+        iteration += 1
+    return True
+
+def find_winner_id(results):
+    for b in results:
+        smallest = None
+        winner_id = None
+        number_of_iteration = results[b]['iteration']
+
+        if not smallest:
+            smallest = number_of_iteration
+            winner_id = b
+        else:
+            if number_of_iteration < smallest:
+                smallest = number_of_iteration
+                winner_id = b
+    
+    return winner_id
+
+
+def get_sum_of_all_unmarked_numbers(board_data):
+    pos = np.where( board_data != 999)
+    location = [item for item in zip(pos[0], pos[1])]
+    sum = 0 
+    for l in location:
+        number = board_data[l[0],l[1]]
+        sum += number
+    return sum
+
+
+
+
+
+# Load Data
+with open('day4_input.txt', 'r') as f:
+    data = [] 
+    for value in f:
+        line = value.split()
+        if len(line) > 0:
+            new_line = [int(i) for i in line]
+            data.append(new_line)
+
+def make_boards(data):
+    array_size = 5
+    boards = []
+    for i in range(0, len(data), array_size):
+        x = np.array(data[i:i+array_size])
+        boards.append(x)
+    return boards
+
+numbers = [69,88,67,56,53,97,46,29,37,51,3,93,92,78,41,22,45,66,13,82,2,7,52,40,18,70,32,95,89,64,84,68,83,26,43,0,61,36,57,34,80,39,6,63,72,98,21,54,23,28,65,16,76,11,20,33,96,4,10,25,30,19,90,24,55,91,15,8,71,99,58,14,60,48,44,17,47,85,74,87,86,27,42,38,81,79,94,73,12,5,77,35,9,62,50,31,49,59,75,1]
+
+boards = make_boards(data)
+for board_id, board_data in enumerate(boards):
+    results = play_game(numbers, board_data, board_id)
+
+winner_board = find_winner_id(results)
+
+for i in results:
+    print(i, results[i])
+board_data = results[winner_board]['board_data']
+winning_number = results[winner_board]['winning_number']
+sum_of_all_unmarked_numbers = get_sum_of_all_unmarked_numbers(board_data)
+final_score = winning_number * sum_of_all_unmarked_numbers
+print(final_score)
+
+

93760f8a46815d5b4ca5febe03a5b1bdd6a2edeb
Alexander Osipov <shuraosipov@gmail.com>
2021-12-08 00:03:26 -0500

    Day 5 - Part One

--- day5.py
+++ day5.py
@@ -11,9 +11,6 @@ with open('day5_input.txt', 'r') as f:
         x2 = int(x2.strip())
         y2 = int(y2.strip())
 
-        entry = x1,y1, "->", x2,y2
-        
-
         if x1 == x2:
            print("Generating points for horisontal line", line)
            for i in range(min(y1,y2),max(y1,y2) + 1):
@@ -21,7 +18,6 @@ with open('day5_input.txt', 'r') as f:
                if point not in result:
                    result[point] = 0
                result[point] += 1
-               print(point, result[point])
 
         elif y1 == y2:
             print("Generating points for vertical line", line)
@@ -29,11 +25,11 @@ with open('day5_input.txt', 'r') as f:
                point = (i,y1)
                
                if point not in result:
-                   result[point] = 1
+                   result[point] = 0
                result[point] += 1
-               print(point, result[point])
 
-#print(result)
+# How many points do at least two lines overlap?
+print(sum(value >= 2  for value in result.values()))
             
 
         

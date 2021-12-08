8731df079ff8775bb4ec1e7f6ea5f4cdb6242302
Alexander Osipov <shuraosipov@gmail.com>
2021-12-08 00:50:27 -0500

    cleanup

--- day5.py
+++ day5.py
@@ -7,11 +7,11 @@ def update_results(point):
 
 def calculate_vertical_lines(x1,x2,y1):
     for i in range(min(x1,x2),max(x1,x2) + 1):
-               update_results((i,y1))
+        update_results((i,y1))
 
 def calculate_horisontal_lines(x1, y1, y2):
     for i in range(min(y1,y2),max(y1,y2) + 1):
-               update_results((x1,i))
+        update_results((x1,i))
 
 def calculate_diagonal_lines(x1,x2,y1,y2):
     dx = x2 - x1

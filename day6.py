f163867cafc1e55129aa5b2ee45e7c1e0c357dce
Alexander Osipov <shuraosipov@gmail.com>
2021-12-09 01:19:25 -0500

    Day 6 - Part One - cleanup

--- day6.py
+++ day6.py
@@ -1,31 +1,12 @@
 
-# 
+from types import resolve_bases
+
+
 def load_input(file) -> list:
     with open(file, 'r') as f:
         line = f.readline()
         result = [int(x) for x in line.split(",")]
         return result
-
-
-
-def calculate_reproduction(fish_list):
-    
-    new_fishes = []
-        
-    for id, timer in enumerate(fish_list):
-        if timer == 0:
-            timer = 6
-            fish_list[id] = timer
-            new_fishes.append(1)
-        else:
-            timer -= 1
-            fish_list[id] = timer
-        
-    if len(new_fishes) > 0:
-        for _ in range(len(new_fishes)):
-            fish_list.append(8)
-    
-    print(f"After {day} days: {len(fish_list)} fish")
 
 
 fish_list = load_input('day6_input.txt')
@@ -35,6 +16,15 @@ for day in range(days + 1):
     if day == 0:
         print("Initial state:", len(fish_list), "fish")
     else:
-        calculate_reproduction(fish_list)
+        
+        results = []
+        for f in fish_list:
+            if f == 0:
+                results.append(6)
+                results.append(8)
+            else:
+                results.append(f-1)
+        fish_list = results
+        print(f"After {day} days: {len(results)} fish")
     
 print(f"After {days} days, there are a total of {len(fish_list)} fish")

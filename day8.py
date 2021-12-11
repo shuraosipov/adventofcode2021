7700815b45fec6307c88d4435c013940d3a17cbf
Alexander Osipov <shuraosipov@gmail.com>
2021-12-11 16:46:24 -0500

    cleanup

--- day8.py
+++ day8.py
@@ -1,4 +1,3 @@
-
 def find_1_4_7_8():
     for signal in signal_patterns:
         if len(signal) in [7,2,3,4]:
@@ -15,7 +14,7 @@ def find_0_2_3_5_6_9():
     for signal in signal_patterns:
         if len(signal) in [5,6]:
             if len(signal) == 5:
-                if set(signal + Y[1]) == set(signal): # find 3
+                if set(signal + Y[1]) == set(signal):
                     Y[3] = signal
                 elif set(signal + Y[4]) == set(Y[8]):
                         Y[2] = signal
@@ -23,7 +22,7 @@ def find_0_2_3_5_6_9():
                     Y[5] = signal
 
             elif len(signal) == 6:
-                if set(signal + Y[1]) != set(signal): # find 6
+                if set(signal + Y[1]) != set(signal):
                     Y[6] = signal
                 elif set(signal + Y[5]) == set(signal):
                     Y[9] = signal
@@ -40,11 +39,12 @@ def find_output():
             if value == nv:
                 output_value.append(str(k))
     value = "".join(output_value)
-    results.append(int(value))
+    results_p2.append(int(value))
 
 
 Y = {}
-results = []
+results_p1 = 0
+results_p2 = []
 
 with open('day8_input.txt', 'r') as f:
     for line in f:
@@ -52,8 +52,13 @@ with open('day8_input.txt', 'r') as f:
         signal_patterns = sorted(new_line[0].split(), key=len)
         output_values = new_line[1].split()
 
+        for value in output_values:
+            if len(value) in [2,4,3,7]:
+                results_p1 += 1
+
         find_1_4_7_8()               
         find_0_2_3_5_6_9()
         find_output()
 
-print(sum(results))
+print("Part One: ", results_p1)
+print("Part Two: ", sum(results_p2))

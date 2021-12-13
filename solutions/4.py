1668334f371ff7d61adbb5c0c5981d4bc0d4c12f
Alexander Osipov <shuraosipov@gmail.com>
2021-12-12 22:33:28 -0500

    cleanup

--- solutions/4.py
+++
@@ -1,9 +0,0 @@
-incomplete = '[({(<(())[]>[[{[]{<()<>>'
-complete = '{([(<{}[<>[]}>{[]{[(<()>'
-close_chars = [')',']','}','>']
-
-
-
-if not any(c in incomplete for c in close_chars):
-    print("ALALA")
-

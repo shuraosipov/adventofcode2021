adc6a11fe7ce11f2957e9960ab67ae491e6e74b4
Alexander Osipov <shuraosipov@gmail.com>
2021-12-14 11:11:54 -0500

    Day 11 - Part One

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,12 +1,18 @@
-from collections import deque
+class Octopus:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.energy_level = grid[x][y]
+        self.adj_points = get_adj_points(x,y)
 
-grid = []
-with open('solutions/day11_input.txt', 'r') as f:
-    for line in f:
-        # print(line.strip())
-        new_line = [int(x) for x in line.strip()]
-        grid.append(new_line)
+    def reset_energy_level(self):
+        grid[self.x][self.y] = 0
+    
+    def increase_energy_level(self):
+        grid[self.x][self.y] += 1
 
+    def get_energy_level(self):
+        return grid[self.x][self.y]
 
 def get_adj_points(x, y):
     adj_points = []
@@ -74,84 +80,49 @@ def increase_grid_by_one():
     for id, i in enumerate(grid):
         grid[id] = [x+1 for x in i]
 
+def flash(oct):
+    global ans
+    ans += 1 
+    oct.reset_energy_level()
+    flushed.append((oct.x,oct.y))
+    increase_adj_energy(oct)
 
-            
-
+def increase_adj_energy(oct):
+    adj_points = get_adj_points(oct.x,oct.y)
     
-
-
-R = len(grid)
-C = len(grid[0])
-
-print(R, C)
-
-print("Before any steps:")
-print_grid()
-
-class Octopus:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.energy_level = grid[x][y]
-        self.flashed = False
-        self.adj_points = get_adj_points(x,y)
-
-    def reset_energy_level(self):
-        grid[self.x][self.y] = 0
-    
-    def increase_energy_level(self):
-        grid[self.x][self.y] += 1
-
-    def get_energy_level(self):
-        return grid[self.x][self.y]
-
-def increase_adj_energy(adj):
-    
-    for points in adj:
+    for points in adj_points:
         x,y  = points[0], points[1]
         oct = Octopus(x,y)
         if (oct.x,oct.y) not in flushed:
             oct.increase_energy_level()
         if oct.get_energy_level() > 9:
-            adj_to_flash.append(oct)
+            flash(oct)
+
+
+    
+if __name__ == '__main__':
+    grid = []
+    with open('solutions/day11_input.txt', 'r') as f:
+        for line in f:
+            grid.append([int(x) for x in line.strip()])
+
+    R = len(grid)
+    C = len(grid[0])
+    ans = 0
+
+    for step in range(1,101):
+        flushed = []
         
-        print_grid()
-        print("Adjacent to flash",adj_to_flash)
-    
-
-def flash(oct):
-    oct.reset_energy_level()
-    flushed.append((oct.x,oct.y))
-
-    print_grid()
-
-    adj_points = get_adj_points(oct.x,oct.y)
-    increase_adj_energy(adj_points)
-
-    #print(adj_to_flash)
-    if len(adj_to_flash) > 0:
-        return flash(adj_to_flash.pop())
-   
-
-    #return True
-
-for step in range(1,3):
-    print("Increased by one")
-    increase_grid_by_one()
-    print_grid()
-    
-    flushed = []
-    adj_to_flash = deque()
-    
-    for r in range(R):
-        for c in range(C):
-            oct = Octopus(r,c)
-            print(oct.energy_level, (oct.x, oct.y), oct.adj_points)
-            if oct.get_energy_level() > 9:
-                flash(oct)
-            
-            
+        increase_grid_by_one()
+        
+        for r in range(R):
+            for c in range(C):
+                oct = Octopus(r,c)
+                if oct.get_energy_level() > 9:
+                    flash(oct)
 
     print('After step :', step)
     print_grid()
+    print("Part One:",ans)
+
     

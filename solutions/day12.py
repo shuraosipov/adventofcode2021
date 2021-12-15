44b8ea6bb64640da6053842cf3ab64d04862caf9
Alexander Osipov <shuraosipov@gmail.com>
2021-12-14 20:50:28 -0500

    Day 12 - WIP

---
+++ solutions/day12.py
@@ -0,0 +1,33 @@
+def load_data():
+    graph = {}
+    with open('solutions/day12_input.txt', 'r') as f:
+        for line in f:
+            start, end = line.strip().split("-")
+            if start not in graph:
+                graph[start] = [end]
+            else:
+                graph[start].append(end)
+
+    return graph
+
+def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if start not in graph:
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+
+graph = load_data()
+print(graph)
+
+print(find_all_paths(graph, 'start', 'end'))
+
+    

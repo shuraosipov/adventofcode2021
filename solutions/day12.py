1cd80501b9798e5489b66daa7bb1f0c4de993098
Alexander Osipov <shuraosipov@gmail.com>
2021-12-15 20:25:17 -0500

    Day 12 - Parts 1 & 2

--- solutions/day12.py
+++ solutions/day12.py
@@ -1,39 +1,36 @@
-def load_data():
-    graph = {}
-    with open('solutions/day12_input.txt', 'r') as f:
-        for line in f:
-            start, end = line.strip().split("-")
-            if start not in graph:
-                graph[start] = [end]
-            else:
-                graph[start].append(end)
+# just copied from here - https://www.youtube.com/watch?v=zjq2fGmz2Sg&ab_channel=JonathanPaulson
 
-    return graph
-
-def find_all_paths(graph, start, end, path=[]):
-        path = path + [start]
-        if start == end:
-            return [path]
-        if start not in graph:
-            return []
-        paths = []
-        for node in graph[start]:
-            if node not in path:
-                newpaths = find_all_paths(graph, node, end, path)
-                for newpath in newpaths:
-                    paths.append(newpath)
-        return paths
+from collections import defaultdict, deque
 
 
-# {'start': ['A', 'b'], 'A': ['c', 'b', 'end'], 'b': ['d', 'end']}
-# start with parent - start
-# for each child get subchilds and print child/subchild relation
-# start,A,c,A,b,A,end
+E = defaultdict(list)
+with open('solutions/day12_input.txt', 'r') as f:
+    for line in f:
+        a,b = line.strip().split("-")
+        E[a].append(b)
+        E[b].append(a)
 
-        
+print(E)
+start = ('start', set(['start']), None)
+ans = 0
+Q = deque([start])
+print(Q)
 
-graph = load_data()
-print(graph)
+while Q:
+    pos, small, twice = Q.popleft()
+    #print(pos, small)
+    if pos == 'end':
+        ans += 1
+        continue
+    
+    for y in E[pos]:
+        if y not in small:
+            new_small = set(small)
+            if y.islower():
+                new_small.add(y)
+            Q.append((y,new_small, twice))
+        elif y in small and twice is None and y not in ['start','end']:
+            Q.append((y,small,y))
 
-print(find_all_paths(graph, "start", "end"))
-
+    
+print(ans)
